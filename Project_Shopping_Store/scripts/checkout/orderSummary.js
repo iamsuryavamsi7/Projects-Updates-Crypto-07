2fa051836644193bc64d95c7a1a700bd94fb650b
Crypto-07 <iamsuryavamsi@gmail.com>
2024-02-18 01:02:56 +0530

    Changes Done

--- Project_Shopping_Store/scripts/checkout/orderSummary.js
+++ Project_Shopping_Store/scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct} from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import {renderPaymentSummary} from "./paymentSummary.js";
 
 const today = dayjs();
 
@@ -153,6 +154,8 @@ export function renderOrderSummary(){
 
                 updateCartQuantity();
 
+                renderPaymentSummary();
+
             })
 
         })
@@ -241,6 +244,8 @@ export function renderOrderSummary(){
                 updateDeliveryOption(productId, deliveryOptionId);
 
                 renderOrderSummary();
+
+                renderPaymentSummary();
 
             })
 
